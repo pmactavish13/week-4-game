bf4fef3fea856d6b0c9edd0709a326ce79251a6d
Paula MacTavish <perilousp@hotmail.com>
2018-02-26 17:22:24 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,17 @@
+// Load all html and css before starting js
 $(document).ready(function() {
 
+// variables
 var winsCounter = [0];
 var lossesCounter = [0];
 var randomTarget = [0];
-var yourScore = [0];
+var yourScore = (0);
 var crystalValue = [];
 var crystalImages = ["assets/images/crystals-1.jpeg", "assets/images/crystals-2.jpeg", "assets/images/crystals-3.jpeg", "assets/images/crystals-4.jpeg"]
+var clickTotal = "";
+var yourTotalScore = "";
 
+// Counters
 $("#winsCounter").append(winsCounter);
 $("#lossesCounter").append(lossesCounter);
 $("#yourScore").append(yourScore);
@@ -22,11 +27,11 @@ for (var i = 0; i < crystalImages.length; i++) {
 randomNumber = Math.floor(Math.random() * 12) +1;
 crystalValue.push(randomNumber);
 }
-
+// Creates buttons for Crystals, adds class, value and css
 for (var i=0; i<crystalValue.length; i++) {
     var crystalButtons = $("<button>");
     crystalButtons.addClass("crystal-button crystal-image") .val(crystalValue[i]);
-   crystalButtons.attr("data-let", crystalValue[i]);
+    // crystalButtons.attr("data-let", crystalValue[i]);
     crystalButtons.css({
       "background-image" : "url('" + crystalImages[i] + "')",
       "background-size" : "100%", "width": "240px", "height": "180px", "margin-right": "35px",
@@ -34,21 +39,38 @@ for (var i=0; i<crystalValue.length; i++) {
     $("#crystals").append(crystalButtons);
 }
 
-function initializeTotalScore() {
-   yourScore = "";
-console.log("Hello");
-$( ".crystal-image").on("click", function() {
+//function initializeTotalScore() {
+  // yourScore = "";
 
-  console.log(this.value);
+//
+$( ".crystal-image").on("click", function() {
+this.value = parseInt(this.value, 10);
+ yourTotalScore  += this.value;
+
+ // console.log(yourTotalScore);
+
+ yourTotalScore = parseInt(yourTotalScore, 10);
+
+ for (var i  = 0; i < yourTotalScore.length; i++){
+
+  total  += yourTotalScore[i];
+
+  console.log (total);
+ }
+//yourTotalScore = clickTotal + clickTotal;
+//$.isNumeric("clickTotal");
+//var result = clickTotal + yourTotalScore
+//yourTotalScore.push(result);
+//newTotal = yourScore;
+ //$("#yourScore").html(yourTotalScore);
+  //console.log(yourTotalScore);
   
 
  }); 
 // //   $("#crystals").append(yourScore);
+//$( "#yourScore" ).replaceWith( "<h2>New heading</h2>" );
 
- 
-
-
-} 
+//}); 
 
 
 
