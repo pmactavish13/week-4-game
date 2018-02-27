5f762b109d243a198da475ba8480376b680339d0
Paula MacTavish <perilousp@hotmail.com>
2018-02-27 17:53:06 -0500

    finished product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,73 @@
 // Load all html and css before starting js
-$(document).ready(function() {
+$(document).ready(function () {
 
-// variables
-var winsCounter = (0);
-var lossesCounter = "";
-var randomTarget = "";
-var yourScore = (0);
-var crystalValue = [];
-var crystalImages = ["assets/images/crystals-1.jpeg", "assets/images/crystals-2.jpeg", "assets/images/crystals-3.jpeg", "assets/images/crystals-4.jpeg"]
-var clickTotal = "";
-var yourTotalScore = "";
+  // variables - global
+  var winsCounter = 0;
+  var lossesCounter = 0;
+  var crystalImages = ["assets/images/crystals-1.jpeg", "assets/images/crystals-2.jpeg", "assets/images/crystals-3.jpeg", "assets/images/crystals-4.jpeg"]
 
-// Counters
-$("#winsCounter").text(winsCounter);
-$("#lossesCounter").text(lossesCounter);
-$("#yourScore").text(yourScore);
+  // re-set Game parameters and clear target screens for each new run-through
+  function startGame() {
+    var randomTarget = 0;
+    var crystalValue = [];
+    var clickTotal = 0;
+    var yourTotalScore = 0;
+    // clears screen of last game parameters
+    $("#randomTarget, #yourScore, #crystals").empty();
+    // scores at start of game round 
+    $("#winsCounter").text(winsCounter);
+    $("#lossesCounter").text(lossesCounter);
+    $("#yourScore").text(yourTotalScore);
+    console.log(randomTarget);
+    console.log(crystalValue);
+    console.log(clickTotal);
+    console.log(yourTotalScore);
 
-function startGame() {
-  //function initializeTotalScore() {
-  // yourScore = "";
-
+    //set game parameters
+    
     // Generate Random Target Number
-randomTarget = Math.floor(Math.random() * 102)+19;
-$("#randomTarget").append(randomTarget);
+    randomTarget = Math.floor(Math.random() * 102) + 19;
+    $("#randomTarget").append(randomTarget);
 
-// Generate Random Number for Crystals
-for (var i = 0; i < crystalImages.length; i++) {
-randomNumber = Math.floor(Math.random() * 12) +1;
-crystalValue.push(randomNumber);
-}
-// Creates buttons for Crystals, adds class, value and css
-for (var i=0; i<crystalValue.length; i++) {
-    var crystalButtons = $("<button>");
-    crystalButtons.addClass("crystal-button crystal-image") .val(crystalValue[i]);
-    // crystalButtons.attr("data-let", crystalValue[i]);
-    crystalButtons.css({
-      "background-image" : "url('" + crystalImages[i] + "')",
-      "background-size" : "100%", "width": "240px", "height": "180px", "margin-right": "35px",
-    }); 
-    $("#crystals").append(crystalButtons);
-}
+    // Generate Random Number for Crystals
+    for (var i = 0; i < crystalImages.length; i++) {
+      randomNumber = Math.floor(Math.random() * 12) + 1;
+      crystalValue.push(randomNumber);
+    }
 
+    // Creates buttons for Crystals, adds class, value and css
+    for (var i = 0; i < crystalValue.length; i++) {
+      var crystalButtons = $("<button>");
+      crystalButtons.addClass("crystal-button crystal-image").val(crystalValue[i]);
+      // crystalButtons.attr("data-let", crystalValue[i]); - another way to assign value
+      crystalButtons.css({
+        "background-image": "url('" + crystalImages[i] + "')",
+        "background-size": "100%", "width": "240px", "height": "180px", "margin-right": "35px",
+      });
+      $("#crystals").append(crystalButtons);
+    }
 
+    // main game play
+    $(".crystal-image").on("click", function () {
+      crystalClick = parseInt(this.value);
+      yourTotalScore += crystalClick;
+      yourTotalScore = parseInt(yourTotalScore);
+      $("#yourScore").text(yourTotalScore);
 
+      target = parseInt(randomTarget);
 
-$( ".crystal-image").on("click", function() {
-crystalClick = parseInt(this.value);
-// console.log(typeof(crystalClick));
- yourTotalScore  += crystalClick;
- //console.log(crystalClick);
+      if (yourTotalScore === target) {
+        winsCounter++;
+        startGame();
+      }
 
-//console.log(yourTotalScore);
-yourTotalScore = parseInt(yourTotalScore, 10);
-$("#yourScore").html(yourTotalScore);
+      else if (yourTotalScore > target) {
+        lossesCounter++;
+        startGame();
+      }
 
-target = parseInt(randomTarget);
-
-if (yourTotalScore === target) {
-winsCounter++;
-$("winsCounter").text(winsCounter);
-
-}
-//$("#winsCounter").append(winsCounter);
-//$("#lossesCounter").append(lossesCounter);
-else if (yourTotalScore > target) {
-lossesCounter--;
-$("lossesCounter").html(lossesCounter);
-console.log (lossesCounter);
-}
-
-
-
- }); 
-}
-startGame();
-});            
-
-
-// // while (condition) { */}
-//  /* //   code block to be executed
-// // 
-
-
-// // If your score = the computer's pick you let them know.
-// //if (parseInt(youScore) === randomNumber) curly
-// //    winsCounter++; 
-// //    lockGame = true;
-// //  curly
-
-//   // If the numbers did not match. You also let them know
-// //  if (parseInt(yourScore) > randomNumber) curly
-// //    lossesCounter++;
-// //    lockGame = true;
+    });
+  }
+  // starts game the first time
+  startGame();
+});
